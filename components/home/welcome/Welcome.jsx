67db7fcf8c55a249c86b952a3a807eb8032743cd
Elem <elem15@yandex.ru>
2023-06-19 23:53:39 +0300

    feat: add text-input fonts

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './welcome.style'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList
+} from 'react-native';
+import { useState } from 'react';
+import styles from './welcome.style';
+import { icons, SIZES } from '../../../constants';
+import { useRouter } from 'expo-router';
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>
+          Hello, Elem
+        </Text>
+        <Text style={styles.welcomeMessage}>
+          Find you perfect job
+        </Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+        <TextInput
+          style={styles.searchInput}
+        />
+        </View>
+      </View>
     </View>
   )
 }
